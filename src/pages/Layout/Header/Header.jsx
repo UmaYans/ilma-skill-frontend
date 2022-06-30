150a00eecba8710ec74a1bbf32f3294dc9c7cb66
Denu <Denu@dota2.ru>
2022-06-30 10:33:20 +0300

    не закончил

--- src/pages/Layout/Header/Header.jsx
+++ src/pages/Layout/Header/Header.jsx
@@ -1,12 +1,39 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import style from "./header.module.css";
 
 const Header = () => {
   return (
-    <header>
-      <NavLink to="/" >Главная</NavLink>
-      <NavLink to="/course">Курсы</NavLink>
-      <NavLink to="/profile">Личный кабинет</NavLink>
+    <header className={style.header}>
+      <div className={style.main}>
+        <div className={style.populite}>
+          <div className={style.denomination}>Iilma-skill</div>
+          <NavLink
+            className={({ isActive }) =>
+              isActive ? style.slova2 : style.slova
+            }
+            to="/"
+          >
+            Главная
+          </NavLink>
+          <NavLink
+            className={({ isActive }) =>
+              isActive ? style.slova2 : style.slova
+            }
+            to="/course"
+          >
+            Курсы
+          </NavLink>
+          <NavLink
+            className={({ isActive }) =>
+              isActive ? style.slova2 : style.slova
+            }
+            to="/profile"
+          >
+            Личный кабинет
+          </NavLink>
+        </div>
+      </div>
     </header>
   );
 };
